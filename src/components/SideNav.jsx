aa21a2c630742a6212eabeed740a702cb1798b93
Kunal-Sarpal <sarpalkunal7@gmail.com>
2024-04-11 21:56:41 +0530

    Minor updates

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -4,40 +4,40 @@ import { CreateContext } from "../context";
 import "./SideNav.css";
 function SideNav() {
   // const [state,setState] = useState(true);
-  const {isTrue} = useContext(CreateContext) ;
+  const { isTrue } = useContext(CreateContext);
   return (
     <div>
-      
       {" "}
-      <div>
-        {!isTrue ? (<div className=" sidenav w-[100vw]   h-screen  fixed duration-300 translate-x-[-100vw]">
-          <div className=" w-72 h-screen bg-zinc-900 text-white mt-11  fixed ">
-          <div className="border-b-[1px] mx-3 px-4 py-5 ">Home</div>
-          <div className="border-b-[1px] mx-3 px-4 py-5 ">Your Channel</div>
-          <div className="border-b-[1px] mx-3 px-4 py-5 ">Subscription</div>
-          <div className="border-b-[1px] mx-3 px-4 py-5 ">History</div>
-          <div className="border-b-[1px] mx-3 px-4 py-5 ">Playlist</div>
-          <div className="border-b-[1px] mx-3 px-4 py-5 ">Trending</div>
-          <div className="border-b-[1px] mx-3 px-4 py-5 ">Gaming</div>
-          <div className="border-b-[1px] mx-3 px-4 py-5 ">Movies</div>
-        </div>
-        </div>
-
-        
-        
-      ):(<div className=" sidenav w-[100vw]  fixed  h-screen duration-300">
-      <div className=" w-72 h-screen bg-zinc-900 text-white mt-11  fixed ">
-      <div className="border-b-[1px] mx-3 px-4 py-5 ">Home</div>
-      <div className="border-b-[1px] mx-3 px-4 py-5 ">Your Channel</div>
-      <div className="border-b-[1px] mx-3 px-4 py-5 ">Subscription</div>
-      <div className="border-b-[1px] mx-3 px-4 py-5 ">History</div>
-      <div className="border-b-[1px] mx-3 px-4 py-5 ">Playlist</div>
-      <div className="border-b-[1px] mx-3 px-4 py-5 ">Trending</div>
-      <div className="border-b-[1px] mx-3 px-4 py-5 ">Gaming</div>
-      <div className="border-b-[1px] mx-3 px-4 py-5 ">Movies</div>
-    </div>
-    </div>)}
-       
+      <div className="flex">
+        {!isTrue ? (
+          <div className="sidenav w-full fixed h-full  duration-500 translate-x-[-100vw]  backdrop-blur-2xl bg-zinc-800">
+            <div className=" w-72 h-screen bg-zinc-900 text-white mt-11  fixed ">
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Home</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Your Channel</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Subscription</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">History</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Playlist</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Trending</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Gaming</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Movies</div>
+            </div>
+          </div>
+        ) : (
+          <div className=" fixed  w-[100vw] backdrop-blur-sm h-full duration-300 translate-x-[0]">
+            <div className=" w-72 h-screen bg-zinc-900 text-white mt-11  fixed ">
+              <div className="border-b-[1px] mx-3 px-4 py-5 hover:ease-in cursor-pointer">
+                Home
+              </div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Your Channel</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Subscription</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">History</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Playlist</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Trending</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Gaming</div>
+              <div className="border-b-[1px] mx-3 px-4 py-5 ">Movies</div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
