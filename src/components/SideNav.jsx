c3ab505d90955f9b9544718fd9196a1015540734
Kunal-Sarpal <sarpalkunal7@gmail.com>
2024-04-11 21:22:15 +0530

    Minor updates

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -10,7 +10,7 @@ function SideNav() {
       
       {" "}
       <div>
-        {!isTrue && (<div className=" sidenav w-[100vw]   h-screen  fixed">
+        {!isTrue ? (<div className=" sidenav w-[100vw]   h-screen  fixed duration-300 translate-x-[-100vw]">
           <div className=" w-72 h-screen bg-zinc-900 text-white mt-11  fixed ">
           <div className="border-b-[1px] mx-3 px-4 py-5 ">Home</div>
           <div className="border-b-[1px] mx-3 px-4 py-5 ">Your Channel</div>
@@ -25,7 +25,18 @@ function SideNav() {
 
         
         
-      ) }
+      ):(<div className=" sidenav w-[100vw]  fixed  h-screen duration-300">
+      <div className=" w-72 h-screen bg-zinc-900 text-white mt-11  fixed ">
+      <div className="border-b-[1px] mx-3 px-4 py-5 ">Home</div>
+      <div className="border-b-[1px] mx-3 px-4 py-5 ">Your Channel</div>
+      <div className="border-b-[1px] mx-3 px-4 py-5 ">Subscription</div>
+      <div className="border-b-[1px] mx-3 px-4 py-5 ">History</div>
+      <div className="border-b-[1px] mx-3 px-4 py-5 ">Playlist</div>
+      <div className="border-b-[1px] mx-3 px-4 py-5 ">Trending</div>
+      <div className="border-b-[1px] mx-3 px-4 py-5 ">Gaming</div>
+      <div className="border-b-[1px] mx-3 px-4 py-5 ">Movies</div>
+    </div>
+    </div>)}
        
       </div>
     </div>
